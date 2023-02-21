652b332c76bb51435703d1845139903e89e908e9
Sebastian Walter <sebwalter@gmx.net>
2023-02-21 21:08:38 +0100

    M EvilBaschdi.Settings/ByMachineAndUser/IAppSettingByKey.cs  D EvilBaschdi.Settings/Writable/Internal/Extensions.cs

--- EvilBaschdi.Settings/ByMachineAndUser/IAppSettingByKey.cs
+++ EvilBaschdi.Settings/ByMachineAndUser/IAppSettingByKey.cs
@@ -5,9 +5,11 @@
 public interface IAppSettingByKey : IValueFor<string, string>, IRunFor2<string, string>
 {
     /// <summary>Value</summary>
+    // ReSharper disable once UnusedMember.Global
     TOut ValueFor<TOut>(string key);
 
     /// <summary>
     /// </summary>
+    // ReSharper disable once UnusedMember.Global
     void RunFor<TIn>(string key, TIn value);
 }
